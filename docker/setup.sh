f6b4035f5c574f85024493b6493e466cbb108bd2
Jonathan Chua <83998855+Airvander@users.noreply.github.com>
2025-05-16 22:53:00 +0800

    Create setup.sh

---
+++ docker/setup.sh
@@ -0,0 +1,11 @@
+Add a docker_setup.sh script to automate Docker installation:
+
+bash
+#!/bin/bash
+# Install Docker
+curl -fsSL https://get.docker.com | sh
+sudo usermod -aG docker $USER
+newgrp docker
+
+# Install Docker Compose
+sudo apt install -y docker-compose-plugin
